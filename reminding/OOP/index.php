1ebfae72869358ac1fff985b5c9289f1a1022f18
Stefan Oljacic <djedpool@gmail.com>
2020-01-23 17:06:54 +0100

    Working on inheritance.

--- reminding/OOP/index.php
+++ reminding/OOP/index.php
@@ -13,24 +13,51 @@
         public function addFriend() {
             return "$this->username added a new friend";
         }
+
+        public function getEmail() {
+            return $this->email;
+        }
+
+        public function setEmail($email) {
+            if(strpos($email, '@') > -1) {
+                $this->email = $email;
+            }
+        }
+    }
+
+    class AdminUser extends User {
+        public $level;
+
+        public function __construct($username, $email, $level) {
+            $this->level = $level;
+            parent::__construct($username, $email);
+        }
     }
 
     $userOne = new User('Stef', 'stef@netnjinja.co.uk');
     $userTwo = new User('Nik', 'nik@netnjinja.co.uk');
+    $userThree = new AdminUser('Sandvic', 'sandvic@netninja.co.uk', 5);
 
-    echo $userOne->username.'<br>';
+    var_dump($userThree);
+    echo $userThree->getEmail();
+
+//    echo $userOne->username.'<br>';
     // echo $userOne->email.'<br>'; // if is private we can no longer do this
-    echo $userOne->addFriend().'<br>';
+//    echo $userOne->addFriend().'<br>';
 
     // $userTwo->username = 'Covek';
     // $userTwo->email = 'covek@ryu.com';
-    echo $userTwo->username.'<br>';
+//    echo $userTwo->username.'<br>';
     // echo $userTwo->email.'<br>';
-    echo $userTwo->addFriend().'<br>';
+//    echo $userTwo->addFriend().'<br>';
     // echo 'This is class of: '.get_class($userOne); // check what is class
     // Mehtod that checks few thing about classes that u inherti from otherelse code
-    var_dump(get_class_vars('User'));  // gets properties
-    var_dump(get_class_methods('User')); // gets methods
+//    var_dump(get_class_vars('User'));  // gets properties
+//    var_dump(get_class_methods('User')); // gets methods
+    echo '<br><br>';
+//    $userOne->setEmail('test@gmail.com');
+//    echo  $userOne->getEmail().'<br>';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
