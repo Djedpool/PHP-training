154d4a5d71a9caea67fb0234fb5b8f2c855dbd4e
Stefan Oljacic <djedpool@gmail.com>
2020-02-21 10:21:20 +0100

    Exapnding skils/

---
+++ basic-mvc/app/controllers/PrcController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stef
+ * Date: 2/21/2020
+ * Time: 10:09 AM
+ */
+
+class PrcController
+{
+
+}
---
+++ basic-mvc/app/core/App.php
@@ -0,0 +1,38 @@
+<?php
+
+class App {
+
+    protected $controller = 'HomeController';
+    protected $method = 'index';
+    protected $params = [];
+
+    public function __construct()
+    {
+        $url = $this->parseUrl();
+
+        print_r($url);
+
+        if(file_exists('../app/controllers/' . ucfirst($url[0]) . 'Controller' . '.php')) {
+            $this->controller = ucfirst($url[0]) . 'Controller';
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+
+        $this->controller = new $this->controller;
+
+        if(isset($url[1])) {
+            if(method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+    }
+
+    public function parseUrl()
+    {
+        if(isset($_GET['url'])) {
+            return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
+    }
+}
